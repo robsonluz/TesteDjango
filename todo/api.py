c1068222f3c1bd1b3d0f8ff387f357386615e2df
Robson João Padilha da Luz <robson.luz@gmail.com>
2021-10-28 10:50:28 +0000

    exemplo de serializer com relacionamento

--- todo/api.py
+++ todo/api.py
@@ -1,6 +1,6 @@
 
-from todo.models import Duvida
 from rest_framework import routers, serializers, viewsets
+from todo.models import Duvida, Filme, Ator
 
 # Serializers define the API representation.
 class DuvidaSerializer(serializers.ModelSerializer):
@@ -13,6 +13,26 @@ class DuvidaViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Duvida.objects.all()
     serializer_class = DuvidaSerializer
 
+
+# Serializers define the API representation.
+class AtorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ator
+        fields = ['id', 'nome']
+
+# Serializers define the API representation.
+class FilmeSerializer(serializers.ModelSerializer):
+    atores = AtorSerializer(many=True, read_only=True)
+    class Meta:
+        model = Filme
+        fields = ['id', 'titulo', 'sinopse', 'atores']
+
+# ViewSets define the view behavior.
+class FilmeViewSet(viewsets.ModelViewSet):
+    queryset = Filme.objects.all()
+    serializer_class = FilmeSerializer        
+
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'duvidas', DuvidaViewSet)
+router.register(r'filmes', FilmeViewSet)
