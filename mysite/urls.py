75473dcd3e501bdad319a9e5b4fb8185495b2a70
Robson João Padilha da Luz <robson.luz@gmail.com>
2021-11-04 13:16:16 +0000

    exemplo de upload

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from todo.views import todoView, addTodo, deleteTodo, coverImage
 from todo.api import router
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),    
