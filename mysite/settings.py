55d20cf4b8c7b7849249788af9dd2745d83140dc
Robson João Padilha da Luz <robson.luz@gmail.com>
2022-05-09 11:29:56 +0000

    configuracao banco de dados elephantsql

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,12 +78,36 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': os.environ['dbname'],
+
+        'USER': os.environ['dbuser'],
+
+        'PASSWORD': os.environ['dbpassword'],
+
+        'HOST': os.environ['dbhost'],
+
+        'PORT': '5432',
+
     }
+
 }
+
+
+
 
 
 # Password validation
